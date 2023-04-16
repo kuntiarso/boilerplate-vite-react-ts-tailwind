f193ebe95024413dff0b0f1be52f300051f16412
kuntiarso <gilang.grk17@gmail.com>
2023-04-16 23:41:14 +0700

    feat: init project

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_AUTHOR: string
+  readonly VITE_API_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import eslintPlugin from '@nabla/vite-plugin-eslint'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
+import viteSvgr from 'vite-plugin-svgr'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+// ref --> https://vitejs.dev/config
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+
+  return {
+    plugins: [react(), eslintPlugin(), viteSvgr(), tsConfigPaths()],
+    server: {
+      host: process.env.SERVER_HOST,
+      port: parseInt(process.env.SERVER_PORT)
+    },
+    resolve: {
+      alias: {
+        // '/': path.resolve(__dirname, './public'),
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  }
+})
